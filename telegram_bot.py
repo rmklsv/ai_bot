1a68710f35e23690508a95a784c4348d2df1ff76
rmklsv <125547175+rmklsv@users.noreply.github.com>
2023-02-26 12:54:38 +0300

    Updated photo handler, text recognition function v.0.01

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,5 +1,5 @@
 from aiogram import Bot, Dispatcher, executor, types
-from aiogram.types import Message
+from text_recognition import text_recognition
 
 import config
 
@@ -8,18 +8,27 @@ bot = Bot(token=config.TOKEN)
 dp = Dispatcher(bot)
 
 
-@dp.message_handler(commands=['start'])
-#Bot description
-async def start_command(message: types.Message):
-    await message.answer('This bot gives the answers to tasks '
-                         'using GPT chat. Just send a photo with '
-                         'your task to recognize text.')
+@dp.message_handler(commands=["start"])
+#Welcome message
+async def start_command(msg: types.Message):
+    await msg.answer("Hello!\nHow can I help you today?")
 
-@dp.message_handler(content_types=['photo'])
+@dp.message_handler(commands=["help"])
+#Bot functional description
+async def help_command(msg: types.Message):
+    await msg.answer("This bot gives the answers to tasks "
+                         "using GPT chat. Just send a photo with "
+                         "your task to recognize text.")
+
+@dp.message_handler(content_types=["photo"])
 #Photo from user call the function to recognize text
-async def photo_handler(message: Message):
-    recognized_text = recognize_text(message.photo)
-    await message.answer(recognized_text)
+async def photo_handler(msg: types.Message):
+    file_id = msg.photo[-1].file_id
+    photo = await bot.get_file(file_id)
+    photo_path = "photos/%s.jpg" % (file_id)
+    await photo.download(destination_file=photo_path)
+    recognized_text = text_recognition(photo_path)
+    await msg.answer(recognized_text)
 
 
 if __name__ == '__main__':
--- text_recognition.py
+++ text_recognition.py
@@ -1,17 +1,17 @@
 import cv2
 import pytesseract
 
+#Path to the tesseract OCR app
 pytesseract.pytesseract.tesseract_cmd = "tesseract\\tesseract.exe"
 
-def text_recognition(image) -> str:
-    try:
-        image = cv2.imread('image.png')
-        tempImage = cv2.cvtColor(tempImage, cv2.COLOR_BGR2RGB)
-        tempResult = pytesseract.image_to_string(image, lang = "rus+eng")
-        result = ""
-        for x in tempResult:
-            if x == "": continue
-            result += x
-        return result
-    except:
-        return 'Failed to recognize, please send another image.'
+# Define the text recognition function
+def text_recognition(img) -> str:
+    pre_img = cv2.imread(img)
+    text = pytesseract.image_to_string(pre_img)
+    return text
+
+
+
+
+
+
